2a68f98dc53bfbf58b5e25f6a23742730605b7ec
Sundar Nallalagappan <nsundar.1990@gmail.com>
2022-10-16 12:44:10 +0530

    Folder structure completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Sairam",
+    author_email="nsundar.1990@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    
+)
